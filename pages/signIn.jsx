e6f4e8c58a0517dce355410a0db4318daf1ef5ad
Diego Viana <diegovianaf7@gmail.com>
2022-10-13 12:41:16 -0300

    change name file and remove comments

--- pages/signIn.jsx
+++ pages/signIn.jsx
@@ -11,7 +11,6 @@ import {
   TextField,
   Typography 
 } from '@mui/material'
-// import { makeStyles } from '@mui/styles'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 
 function Copyright() {
@@ -27,28 +26,7 @@ function Copyright() {
   )
 }
 
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main,
-//   },
-//   form: {
-//     width: '100%', // Fix IE 11 issue.
-//     marginTop: theme.spacing(1),
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2),
-//   },
-// }))
-
 export default function SignIn() {
-  // const classes = useStyles()
 
   return (
     <Container component="main" maxWidth="xs">
