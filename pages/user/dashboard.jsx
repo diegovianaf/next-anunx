6cd77ea70bc87be6f530bd94e722368d53f1ecaf
Diego Viana <diegovianaf7@gmail.com>
2022-10-24 11:28:59 -0300

    add products container

--- pages/user/dashboard.jsx
+++ pages/user/dashboard.jsx
@@ -1,13 +1,132 @@
-import { Button, Container, ThemeProvider, Typography } from "@mui/material"
-import TemplateDefault from "../../src/templates/Default"
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  ThemeProvider,
+  Typography
+} from '@mui/material'
+
+import TemplateDefault from '../../src/templates/Default'
 
 const Home = () => {
   return (
     <TemplateDefault>
       <ThemeProvider >
-        <Container maxWidth="sm" style={{paddingTop: '2rem', gap: '1rem'}} align="center">
+        <Container maxWidth="sm" style={{paddingTop: '2rem'}} align="center">
           <Typography variant="h2">My Products</Typography>
-          <Button variant="contained" color="primary">Add new Product</Button>
+          <Button variant="contained" color="primary" style={{marginTop: '1rem'}}>Add new Product</Button>
+        </Container>
+        <Container maxWidth="md" style={{paddingTop: '3rem'}}>
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={"https://source.unsplash.com/random"}
+                  alt="Image Title"
+                />
+                <CardContent>
+                  <Typography variant="h5">
+                    Product X
+                  </Typography>
+                  <Typography>
+                    $ 60.00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                  <Button size="small" color="primary">
+                    Remove
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={"https://source.unsplash.com/random"}
+                  alt="Image Title"
+                />
+                <CardContent>
+                  <Typography variant="h5">
+                    Product X
+                  </Typography>
+                  <Typography>
+                    $ 60.00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                  <Button size="small" color="primary">
+                    Remove
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={"https://source.unsplash.com/random"}
+                  alt="Image Title"
+                />
+                <CardContent>
+                  <Typography variant="h5">
+                    Product X
+                  </Typography>
+                  <Typography>
+                    $ 60.00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                  <Button size="small" color="primary">
+                    Remove
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={"https://source.unsplash.com/random"}
+                  alt="Image Title"
+                />
+                <CardContent>
+                  <Typography variant="h5">
+                    Product X
+                  </Typography>
+                  <Typography>
+                    $ 60.00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                  <Button size="small" color="primary">
+                    Remove
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
         </Container>
       </ThemeProvider>
     </TemplateDefault>
