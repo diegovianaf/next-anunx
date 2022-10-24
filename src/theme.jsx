285fba8d6a43a71bc973ead230a753b4251b959d
Diego Viana <diegovianaf7@gmail.com>
2022-10-24 10:55:53 -0300

    change theme color

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,14 +1,14 @@
-import { createTheme } from '@mui/material/styles'
+import { createTheme } from '@mui/material'
 import { red } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#000',
     },
     secondary: {
-      main: '#19857b',
+      main: '#7415e0',
     },
     error: {
       main: red.A400,
