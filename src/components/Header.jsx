5871ef3bf099075077e11bdfa7365c4ddb5ab75c
Diego Viana <diegovianaf7@gmail.com>
2022-11-01 16:39:55 -0300

    add container and user menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,85 @@
-import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
+import { AccountCircle } from '@mui/icons-material'
+import {
+  AppBar,
+  Avatar,
+  Button,
+  Container,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography
+} from '@mui/material'
+import { makeStyles } from '@mui/styles'
+import Link from 'next/link'
+import { useState } from 'react'
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  userName: {
+    marginLeft: 6,
+  },
+  divider: {
+    margin: '8px 0',
+  },
+}))
 
 export default function ButtonAppBar() {
+  const classes = useStyles()
+  const [anchorUserMenu, setAnchorUserMenu] = useState(false)
+
+  const openUserMenu = Boolean(anchorUserMenu)
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+        <Container maxWidth="lg">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Anunx
+            </Typography>
+            <Link href="/user/publish" passHref>
+              <Button color="inherit" variant="outlined">
+                Advertise and Sell
+              </Button>
+            </Link>
+            <IconButton color="secondary" onClick={(e) => setAnchorUserMenu(e.currentTarget)}>
+              {
+                true === false
+                ? <Avatar src="" />
+                : <AccountCircle />
+              }
+              <Typography variant="subtitle2" color="secondary" className={classes.userName}>
+                Diego Viana
+              </Typography>
+            </IconButton>
+            <Menu
+              anchorEl={anchorUserMenu}
+              open={openUserMenu}
+              onClose={() => setAnchorUserMenu(null)}
+              anchorOrigin={{
+                horizontal: 'right',
+                vertical: 'top',
+              }}
+            >
+              <Link href="/user/dashboard" passHref>
+                <MenuItem>My Advertisements</MenuItem>
+              </Link>
+              <Link href="/user/publish" passHref>
+                <MenuItem>Publish new Advertisement</MenuItem>
+              </Link>
+              <Divider className={classes.divider} />
+              <MenuItem>Log out</MenuItem>
+            </Menu>
+          </Toolbar>
+        </Container>
       </AppBar>
-    </Box>
+    </>
   )
 }
